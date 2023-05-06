ca4753b6095aef81d756ca4653826b4968de8ef7
KevinMembreve <kevinmembreve32@gmail.com>
2023-05-07 00:59:48 +0800

    modified vite.config again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'client/main.jsx',
+        main: 'src/main.jsx',
       },
     },
   },
-  build: {
-    outDir: 'dist',
-  },
-})
+});
