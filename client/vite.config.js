3c5c07550daff000889d402da8bc9e39d6dc2840
KevinMembreve <kevinmembreve32@gmail.com>
2023-05-07 00:46:06 +0800

    modified vite.config again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,5 +9,9 @@ export default defineConfig({
         main: 'client/index.html',
       },
     },
+  },
+  // add the output directory configuration below
+  build: {
+    outDir: 'dist',
   },
 })
