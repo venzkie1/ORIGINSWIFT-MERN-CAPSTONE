720e17e76d70a58282bb56607da833b7c517f67b
KevinMembreve <kevinmembreve32@gmail.com>
2023-05-11 17:58:16 +0800

    modified newRequest baseURL

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:8800/api/", 
+    baseURL: "https://645cb2b7d16977781bbadb6c--candid-trifle-f5177a.netlify.app/api/", 
     withCredentials: true,
 });
 
