5b6262bb03e41041c5de19c257ffbdda54e9d52c
KevinMembreve <kevinmembreve32@gmail.com>
2023-05-11 18:24:40 +0800

    tried fixing bug

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "https://645cb2b7d16977781bbadb6c--candid-trifle-f5177a.netlify.app/", 
+    baseURL: "/", 
     withCredentials: true,
 });
 
