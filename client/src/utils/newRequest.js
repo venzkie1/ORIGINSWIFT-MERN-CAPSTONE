348dc9e0aaf8088c03ef379cb855450207c6b700
KevinMembreve <kevinmembreve32@gmail.com>
2023-05-11 18:25:41 +0800

    reverted everything

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "/", 
+    baseURL: "http://localhost:8800/api/", 
     withCredentials: true,
 });
-
 
 export default newRequest;
